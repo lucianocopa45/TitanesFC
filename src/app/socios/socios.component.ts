e1443dbdab1f546506890ee42cf4ea340afcd0bc
lucianocopa45 <45108640@ifts24.edu.ar>
2025-05-25 17:25:04 -0300

    Agregue la navegacion, el codigo base del home, y los componenetes Socios y Profesores

---
+++ src/app/socios/socios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-socios',
+  imports: [],
+  templateUrl: './socios.component.html',
+  styleUrl: './socios.component.css'
+})
+export class SociosComponent {
+
+}
