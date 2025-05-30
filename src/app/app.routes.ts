2da8de1a62d52a2d364e46502a891a6b61a32689
lucianocopa45 <45108640@ifts24.edu.ar>
2025-05-29 21:27:02 -0300

    Agregue los reportes de socios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { SociosComponent } from './socios/socios.component';
 import { ProfesoresComponent } from './profesores/profesores.component';
 import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
+import { ReportesComponent } from './reportes/reportes.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {path: 'socios',component: SociosComponent},
     {path: 'profesores', component: ProfesoresComponent},
+    {path: 'reportes', component: ReportesComponent},
     {path: 'miPerfil', component: MiPerfilComponent}
 ];
