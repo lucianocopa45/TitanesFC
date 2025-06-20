8aca36a40c14c16680fe79c9f2c9406eb24b9ec5
lucianocopa45 <45108640@ifts24.edu.ar>
2025-06-20 12:26:26 -0300

    Cambie la navegacion para que me mande al componente de SocioLayout o AdminLayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,36 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+
 import { SociosComponent } from './socios/socios.component';
 
 import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
 import { ReportesComponent } from './reportes/reportes.component';
 import { ProfesorComponent } from './profesor/profesor.component';
+import { HomeAdminComponent } from './home-admin/home-admin.component';
+import { InicioSocioComponent } from './inicio-socio/inicio-socio.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { SocioLayoutComponent } from './socio-layout/socio-layout.component';
 
 
 export const routes: Routes = [
-
-    { path: '', component: HomeComponent },
-    {path: 'socios',component: SociosComponent},
-    {path: 'profesores', component: ProfesorComponent},
-    {path: 'reportes', component: ReportesComponent},
-    {path: 'miPerfil', component: MiPerfilComponent}
-
-];
+  {
+    path: '',
+    component: SocioLayoutComponent,
+    children: [
+      { path: '', component: InicioSocioComponent },
+      { path: 'panel', component: InicioSocioComponent },
+      { path: 'miPerfil', component: MiPerfilComponent },
+      //{ path: '**', redirectTo: 'panel', pathMatch: 'full' }
+    ]
+  },
+{
+  path: 'home',
+  component: AdminLayoutComponent,
+  children: [
+    { path: '', component: HomeAdminComponent }, // esto carga en /home
+    { path: 'socios', component: SociosComponent },
+    { path: 'profesores', component: ProfesorComponent },
+    { path: 'reportes', component: ReportesComponent },
+    //{ path: '**', redirectTo: '', pathMatch: 'full' } // esto se pone al final
+  ]
+}
+];  
